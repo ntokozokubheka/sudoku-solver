75be7e54d897adc6ea5bdfec26cf1695d1f0ff38
ntokozo <kubhekantozo@hotmail.com>
2025-03-29 08:28:50 -0500

    updates

--- app.js
+++ app.js
@@ -1,53 +1,52 @@
- const puzzleBoard =document.querySelector('#puzzle');
- const solveButton =document.querySelector('#solve-button');
- const squares =81;
- const submission =[];
- for (let i=0 ; i< squares ;i++ ){
+const puzzleBoard = document.querySelector('#puzzle');
+const solveButton = document.querySelector('#solve-button');
+const squares = 81;
+const submission = [];
+for (let i = 0; i < squares; i++) {
 
-    const inputElement =document.createElement('input');
-     inputElement.setAttribute('type','number');
-     inputElement.setAttribute('min','0');
-     inputElement.setAttribute('max','9');
-     puzzleBoard.appendChild(inputElement);
+  const inputElement = document.createElement('input');
+  inputElement.setAttribute('type', 'number');
+  inputElement.setAttribute('min', '0');
+  inputElement.setAttribute('max', '9');
+  puzzleBoard.appendChild(inputElement);
 }
 
 
-const joinValues =() =>{
-const inputs =document.querySelectorAll('input');
-inputs.forEach(input => {
+const joinValues = () => {
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(input => {
 
-if(input.value)
-{
-    submission.push(input.value);
+    if (input.value) {
+      submission.push(input.value);
 
-} else{
+    } else {
 
-     submission.push('.');
+      submission.push('.');
+    }
+
+  })
+
+  console.log(submission);
 }
 
-})
- 
-console.log(submission);
+const solve = () => {
+
+  const options = {
+    method: 'POST',
+    url: 'https://solve-sudoku.p.rapidapi.com/',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
+    },
+    data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
 }
 
-const solve =() =>{
-    
-    const options = {
-      method: 'POST',
-      url: 'https://solve-sudoku.p.rapidapi.com/',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-        'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
-      },
-      data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
-    };
-    
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
-}
-
-solveButton.addEventListener('click',joinValues);
+solveButton.addEventListener('click', joinValues);
